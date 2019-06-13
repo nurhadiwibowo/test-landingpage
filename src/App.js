585fdf2c60262dde41c116a1e1675993a83847cf
Nurhadi <nurhadi1602@gmail.com>
2019-06-13 21:14:34 +0700

    Start with react app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,164 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faComments,
+  faBullhorn,
+  faPaintBrush,
+  faCubes,
+  faSlidersH,
+  faChartLine } from '@fortawesome/free-solid-svg-icons'
+import {faCopyright} from '@fortawesome/free-regular-svg-icons'
+import { CookiesProvider } from 'react-cookie'
+  const datas = [
+    {
+      title: "Consult",
+      paragraph: "Co-create, design thinking, strengthen infrastructure resist granular. Revolution circular, movements or framework social impact low-hanging fruit. Save the world compelling revolutionary progress.",
+      fa: faComments
+    },
+    {
+      title: "Design",
+      paragraph: "Policymaker collaborates collective impact humanitarian shared value vocabulary inspire issue outcomes agile. Overcome injustice deep dive agile issue outcomes vibrant boots on the ground sustainable.",
+      fa: faPaintBrush
+    },
+    {
+      title: "Develop",
+      paragraph: "Revolutionary circular, movements a or impact framework social impact low-hanging. Save the compelling revolutionary inspire progress. Collective impacts and challenges for opportunities of thought provoking.",
+      fa: faCubes
+    },
+    {
+      title: "Marketing",
+      paragraph: "Peaceful; vibrant paradigm, collaborative cities. Shared vocabulary agile, replicable, effective altruism youth. Mobilize commitment to overcome injustice resilient, uplift social transparent effective.",
+      fa: faBullhorn
+    },
+    {
+      title: "Manage",
+      paragraph: "Change-makers innovation or shared unit of analysis. Overcome injustice outcomes strategize vibrant boots on the ground sustainable. Optimism, effective altruism invest optimism corporate social.",
+      fa: faSlidersH
+    },
+    {
+      title: "Evolve",
+      paragraph: "Activate catalyze and impact contextualize humanitarian. Unit of analysis overcome injustice storytelling altruism. Thought leadership mass incarceration. Outcomes big data, fairness, social game-changer.",
+      fa: faChartLine
+    }
+  ]
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export class App extends React.Component {
+  // constructor(props) {
+  //   super(props)
+  
+  //   this.state = {
+  //      cookies : true //true = Show
+  //   }
+  // }
+  componentDidMount(){
+    let element = document.getElementById("cookie-panel")
+    window.onscroll = function(){
+      if(window.pageYOffset <= 0) {
+        element.style.position = "static"
+      } else if (window !==0){
+        element.style.position = "fixed"
+      }
+    }
+    window.onscroll = function(){
+      if(window.pageYOffset >= 0){
+        document.getElementById("newsletter-panel").classList.add("newsletter-panel-mounting")
+      }
+    }
+  }
+  handleClose = () =>{
+    // this.setState({
+    //   cookies: !this.state.cookies
+    // })
+    let element = document.getElementById("cookie-panel")
+    element.classList.toggle("hide")
+    window.scrollTo(0,0)
+    setTimeout(function(){
+      element.style.display = "none"
+    }, 1300)
+  }
+  newsletterClose = (event) => {
+    let element = document.getElementById("newsletter-panel")
+    element.style.display = "none"
+  }
+  componentWillUnmount() {
+    window.onscroll = null;
+  }
+
+  render(){
+    return (
+      <div className="bg-app" onScroll={this.handleScroll}>
+        <div className="App">
+          <div id="cookie-panel" className={`notification-panel`}>
+            <div className="notification-panel-content">
+              <p className="notification-panel-text">
+                By accessing and using this website, you acknowledge that you have read and understand our <a href="/">Cookie Policy</a>, <a href="/">Privacy Policy</a>, and our <a href="/">Terms of Service</a>.
+              </p>
+              <button className="notification-panel-button" onClick={this.handleClose}>Got It!</button>
+            </div>
+          </div>
+          <div className="hero-shot">
+            <div className="hero-shot-content">
+              <img src={require('./assets/img/y-logo-white.png')} alt="logo"/>
+              <span className="jumbo-hello">
+                Hello! I'm Nurhadi Wibowo
+              </span>
+              <span className="jumbo-skills">
+                Consult, Design, and Develop Websites
+              </span>
+              <p className="jumbo-cta">
+                Have something great in mind? Feel free to contact me.
+              </p>
+              <p className="jumbo-cta">
+                I'll help you to make it happen.
+              </p>
+              <button>
+                LET'S MAKE CONTACT
+              </button>
+            </div>
+          </div>
+          <div className="highlight-panel">
+            <span>How Can I Help You?</span>
+            <p>
+              Our work then targeted, best practices outcomes social innovation synergy.<br/>
+              Venture philanthropy, revolutionary inclusive policymaker relief.<br/>
+              User-centered program areas scale.
+            </p>
+            <div className="card-section">
+              {datas.map((data,index)=>
+              <div className="card">
+                <div className="card-header">
+                  <span>
+                    {data.title}
+                  </span>
+                  <FontAwesomeIcon icon={data.fa} className="fa-icon" />
+                </div>
+                <p>
+                  {data.paragraph}
+                </p>
+              </div>
+              )}
+            </div>
+          </div>
+          <div id="newsletter-panel" className="newsletter-panel">
+            <div className="newsletter-content">
+              <button className="close-button" onClick={this.newsletterClose}>x</button>
+              <span>Get latest updates in web technologies</span>
+              <p>
+                I write articles related to web technologies, such as design trends, development
+                tools, UI/UX case studies and reviews, and more. Sign to my newsletter to get them all.
+              </p>
+              <input type="text" placeholder="Email address ..."/>
+              <button className="newsletter-cta">Count me in!</button>
+            </div>
+          </div>
+          <div className="footer">
+             <span><FontAwesomeIcon icon={faCopyright}/>​ 2019 Nurhadi Wibowo. All rights reserved.​</span>     
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
